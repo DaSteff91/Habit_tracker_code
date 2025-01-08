0408c6bd83d36ffa308dd272c749876874c2accc
DaSteff91 <159436322+DaSteff91@users.noreply.github.com>
2025-01-08 11:25:58 +0100

    Updating dummy data files

--- SQlite/20250103_habit_dummy_data.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO habit (id,name,category,description,created,start,end,importance,repeat,tasks,tasks_description,streak,streak_reset_count,longest_streak) VALUES
-	 (1,'Drink water','Health','2 liters per day, evenly distributed across the whole day','2024-07-20 23:30:12','2024-07-01','2024-09-02','High','Daily',5,'I drank 0,2l of water',1,2,16),
-	 (2,'Smiley','Self development','A smile changes the world','2024-07-20 23:33:03','2024-07-01','2024-09-02','High','Daily',2,'Smile from ear to ear',5,0,5),
-	 (3,'Saint','Self development','Become more religious','2024-07-20 23:35:52','2024-07-01','2024-09-13','Paused','Weekly',1,'Make the creator proud',0,4,0),
-	 (4,'Bookworm','Hobby','Take your time for reading and relax','2024-07-20 23:37:51','2024-07-01','2024-09-13','Low','Weekly',3,'Read a book for 1h',1,1,3),
-	 (5,'Siesta','Joy','Take an intentional nap in the afternoon','2024-07-20 23:42:24','2024-07-01','2024-09-02','Low','Daily',1,'Siesta for 30min',2,5,13);
