47d0d575ef837b8021db9f64e58577e15d1c0e8c
DaSteff91 <159436322+DaSteff91@users.noreply.github.com>
2025-01-11 20:17:52 +0100

    Adding some comments

--- views/task_ui.py
+++ views/task_ui.py
@@ -181,7 +181,7 @@ class TaskUI(BaseUI):
         else:
             selected_rows = self.get_selected_tasks(tasks)
             if selected_rows:
-                task_id_map = {task["row"]: task["id"] for task in tasks}
+                task_id_map = {task["row"]: task["id"] for task in tasks} # Map row index to task ID in the DB
                 if self.process_task_update(selected_rows, task_id_map, action):
                     input("\nPress Enter to continue...")
                     self._clear_screen()
@@ -287,12 +287,12 @@ class TaskUI(BaseUI):
                     related_rows = self.task_controller.get_related_pending_tasks(task_id, task_id_map)
                     if related_rows:
                         action_text = "ignore" if status == "ignore" else "pause"
-                        if not questionary.confirm(  # Changed: Store response
+                        if not questionary.confirm(
                             "\nFound other pending tasks for this habit. {} them as well?".format(action_text),
                             default=False,
                             style=self.style
                         ).ask():
-                            continue  # Changed: Skip to next row if user says no
+                            continue
                         new_rows.extend([r for r in related_rows if r not in new_rows])
             return new_rows
         except Exception as e:
